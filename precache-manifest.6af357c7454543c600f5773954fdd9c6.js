f311a1be7b4dcddf34735738412490ddc1de2aef
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2020-08-18 03:10:50 -0700

    Updates

---
+++ precache-manifest.6af357c7454543c600f5773954fdd9c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2364b617747ae758e1b2f8330e640d1",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "dc3183687ec14ff9cdc5",
+    "url": "/reactjs/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "144750b02acac09399eb",
+    "url": "/reactjs/static/css/main.4591f4f3.chunk.css"
+  },
+  {
+    "revision": "dc3183687ec14ff9cdc5",
+    "url": "/reactjs/static/js/2.21441152.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/reactjs/static/js/2.21441152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144750b02acac09399eb",
+    "url": "/reactjs/static/js/main.7578c9a2.chunk.js"
+  },
+  {
+    "revision": "c0f4b5b4894d3258ede2",
+    "url": "/reactjs/static/js/runtime-main.ee8c4b6a.js"
+  },
+  {
+    "revision": "aabd20751ef4ae0c142c699d2722472e",
+    "url": "/reactjs/static/media/Home-Couple-Optimized.aabd2075.png"
+  }
+]);
